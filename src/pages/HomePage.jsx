d12f76eb10605f2519e90ac1a6d7dcad9b62fa7d
SIRAZ490 <mohamadsrj94@gmail.com>
2026-04-29 11:39:16 +0300

    i removed the "what customers "say section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,6 @@ import HeroSlider from '../components/home/HeroSlider';
 import CategorySection from '../components/home/CategorySection';
 import OffersSection from '../components/home/OffersSection';
 import FeaturedProducts from '../components/home/FeaturedProducts';
-import TestimonialsSection from '../components/home/TestimonialsSection';
 
 export default function HomePage() {
   return (
@@ -11,7 +10,6 @@ export default function HomePage() {
       <CategorySection />
       <OffersSection />
       <FeaturedProducts />
-      <TestimonialsSection />
     </main>
   );
 }
